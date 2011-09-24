1a69f9f2fbaf804ae7e5f92e80f430f14ef3ba80
Damjan Georgievski <gdamjan@gmail.com>
2011-09-25 01:21:11 +0200

    modify the node.js echo server to a funnel to my znc bouncer

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var http = require('http');
+var tls = require('tls');
 
 // 3rd party
 var sockjs = require('sockjs');
@@ -8,11 +9,39 @@ var node_static = require('node-static');
 var sockjs_opts = {sockjs_url: "sockjs.js"};
 
 var sockjs_server = new sockjs.Server(sockjs_opts);
-sockjs_server.on('open', function(conn) {
-   conn.on('message', function(e) {
-      conn.send(e.data);
+
+/*
+ * Once the SockJS connection is established, connect to the irc server and
+ * funnel data back and forward. The web browser will have to handle all of the
+ * irc communication including authorization to the znc server.
+ *
+ * When one of the sockets is closed also close the other socket.
+ */
+sockjs_server.on('open', function(web_sock) {
+   // hardcoded host & port… this is my znc irc bouncer
+   // you should customize this to your own liking
+   var irc_sock = tls.connect(6666, "damjan.softver.org.mk", function () {
+      irc_sock.setEncoding('utf-8');
+      web_sock.on('message', function(msg) {
+         console.log(msg.data);
+         irc_sock.write(msg.data);
+      });
+      irc_sock.on('data', function(data) {
+         console.log(data);
+         web_sock.send(data);
+      });
+      // handle close & errors
+      web_sock.on('close', function(e) {
+         console.log('close ' + web_sock, e);
+         irc_sock.destroySoon();
+      });
+      irc_sock.on('close', function(e) {
+         console.log('irc connection closed');
+         web_sock.close();
+      });
    });
 });
+
 
 var static_directory = new node_static.Server(path.join(__dirname, 'static'));
 
