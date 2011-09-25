d6f4fb6d5e3d1c6e20746bfb8ebfd0cf0e310bc1
Damjan Georgievski <gdamjan@gmail.com>
2011-09-25 02:20:35 +0200

    Start to be usefull
    Show a login screen, then send irc commands to log to my znc bouncer

--- static/webirc.js
+++ static/webirc.js
@@ -1,36 +1,59 @@
 jQuery(function ($) {
-   output = function() {
-     var obj = $('#output');
-     return function (m) {
-        obj.append($("<code>").text(m));
-        obj.append($("<br>"));
-        //obj.scrollTop(obj.scrollTop()+10000);
-        $(window).scrollTop($(window).scrollTop()+10000);
-     }
-   }();
+   // first show a login form
+   TINY.box.show({url:'login.html', close:false, boxid:'tinybox', openjs: function() {
+      $("#login_form").submit(function() {
+         $("#tinybox").html("<div style='text-align: center'><img src='images/preload.gif'></div>");
+         var username = $("#username").val();
+         var password = $("#password").val();
+         connect(username, password);
+         return false;
+      })
+   }})
 
-   var sockjs = new SockJS("http://127.0.0.1:9999/sockjs");
-   sockjs.onopen = function() {
-      output(' [*] Connected (using: '+sockjs.protocol+')');
-   };
-   sockjs.onclose = function(e) {
-      output(' [*] Disconnected ('+e.status + ' ' + e.reason+ ')');
-   };
-   sockjs.onmessage = function(e) {
-      output(e.data);
-   };
+   var view = $('#output');
+   var w = $(window);
+   function output (m) {
+      view.append('<div>' + m + '</div>');
+      w.scrollTop(w.scrollTop()+10000);
+   }
 
-   $('#input').focus();
-   $('#form').submit(function() {
-      var val = $('#input').val();
-      $('#input').val('');
-      var l = ' [ ] sending: ' + JSON.stringify(val);
-      if (sockjs.readyState !== SockJS.OPEN) {
-         l += ' (error, connection not established)';
-      } else {
-         sockjs.send(val);
+
+   function connect (username, password) {
+      var sockjs = new SockJS('/sockjs');
+      function send (data) {
+         sockjs.send(data + "\r\n");
       }
-      console.log(l);
-      return false;
-   });
-});
+      sockjs.onopen = function() {
+         TINY.box.hide();
+         $('#input_line').focus();
+         output(' [*] Connected (using: '+sockjs.protocol+')');
+         // FIXME this delay of 100ms is a hack around stupid behaviour in sockjs-node,
+         // will see.
+         setTimeout(function() {
+            send("PASS " + username + ":" + password);
+            send("NICK unimportant");
+            send("USER unimportant");
+         }, 100);
+      };
+      sockjs.onclose = function(e) {
+         output(' [*] Disconnected ('+e.status + ' ' + e.reason+ ')');
+      };
+      sockjs.onmessage = function(msg) {
+         output(msg.data.trim());
+      };
+
+      $('#input_form').submit(function() {
+         var val = $('#input_line').val();
+         $('#input_line').val('');
+         var l = ' [ ] sending: ' + JSON.stringify(val);
+         if (sockjs.readyState !== SockJS.OPEN) {
+            l += ' (error, connection not established)';
+         } else {
+            send(val);
+         }
+         console.log(l);
+         return false;
+      });
+      return sockjs;
+   }
+})
